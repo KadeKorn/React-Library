8d4d911e71d65f4c69494906607a11b06744fe63
Kade <kadekorn@gmail.com>
2023-08-26 16:48:40 -0700

    This has been great. I make another component (highlights) and a folder (ui) to make my code more flush. I kept having an error, so I used chatgpt and it gave me a revised code which fixed it. I was using incorrect naming for the icons. Great experience

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt, faBookOpen, faTags } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import Highlight from './ui/highlight';
 
 const Highlights = () => {
   return (
@@ -10,33 +12,21 @@ const Highlights = () => {
             Why choose <span className="purple">Library</span>
           </h2>
           <div className="highlight__wrapper">
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="bolt" />
-              </div>
-              <h3 className="highlight__subtitle">Easy and Quick</h3>
-              <p className="highlight__para">
-                Get access to the book you purchased online instantly.
-              </p>
-            </div>
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="book-open" />
-              </div>
-              <h3 className="highlight__subtitle">10,0000+ Books</h3>
-              <p className="highlight__para">
-                Library has books in all your favorite categories
-              </p>
-            </div>
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="tags" />
-              </div>
-              <h3 className="highlight__subtitle">Affordable</h3>
-              <p className="highlight__para">
-                Get your hands on popular books for as little as $10
-              </p>
-            </div>
+            <Highlight
+              icon={<FontAwesomeIcon icon={faBolt} />}
+              title="Easy and Quick"
+              para="Get access to the book you purchased online instantly"
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon={faBookOpen} />}
+              title="10,000+ Books"
+              para="Library has books in all your favorite categories"
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon={faTags} />}
+              title="Affordable"
+              para="Get your hands on popular books for as little as $10"
+            />
           </div>
         </div>
       </div>
@@ -45,3 +35,50 @@ const Highlights = () => {
 };
 
 export default Highlights;
+
+
+
+
+
+
+
+
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import React from "react";
+// import Highlight from './ui/highlight'
+
+// const Highlights = () => {
+//   return (
+//     <section id="highlights">
+//       <div className="container">
+//         <div className="row">
+//           <h2 className="section__title">
+//             Why choose <span className="purple">Library</span>
+//           </h2>
+//           <div className="highlight__wrapper">
+//             <Highlight
+//               icon={<FontAwesomeIcon icon="bolt" />}
+//               title="Easy and Quick"
+//               para="Get access to the book you purchased online instantly"
+//             />
+//              <div className="highlight__wrapper">
+//             <Highlight
+//               icon={<FontAwesomeIcon icon="book-open" />}
+//               title="10,000+ Books"
+//               para="Library has books in all your favorite categories"
+//             />
+//              <div className="highlight__wrapper">
+//             <Highlight
+//               icon={<FontAwesomeIcon icon="tags" />}
+//               title="Affordable"
+//               para="Get your hands on popular books for as little as $10"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Highlights;
