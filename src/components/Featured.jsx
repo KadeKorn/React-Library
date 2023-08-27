b2675e89c8e090eb7cc833c21492db1e648b3379
Kade <kadekorn@gmail.com>
2023-08-27 07:12:01 -0700

    this is where things get interesting

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
+    <section id="features"> 
+    {/* ideally this should be Featured or the component should have been named features, but the css is already witten */}
+        <div className="containter">
+            <div className="row">
+                <h2 className="section__title">
+                    Featured <span className="purple">Books</span>
+                </h2>
+            </div>
+        </div>
+    </section>
+    )
 }
 
 export default Featured
