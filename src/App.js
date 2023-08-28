3870a1cf9c91aab40241c1f38dc5fe81315c7fe6
Kade <kadekorn@gmail.com>
2023-08-28 07:22:08 -0700

    just finished up with adding the book page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer.jsx";
 import Nav from "./components/Nav.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" exact Component={Home}></Route>
+          <Route path="/books" Component={Books}></Route>
         </Routes>
         <Footer />
       </div>
