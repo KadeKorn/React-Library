1a12a6f5db41f168cf06a930ac21e9e54960e55c
Kade <kadekorn@gmail.com>
2023-08-30 19:53:55 -0700

    Finished up making the book info page and now am on to fleshing it out/ making it more dynamic

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/ui/Rating";
 
-const BookInfo = ({book}) => {
+const BookInfo = ({ book }) => {
   return (
     <div id="books__body">
-        <main id="books__main">
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        
-                    </div>
-                </div>
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
             </div>
-        </main>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img
+                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
+                  alt=""
+                />
+              </figure>
+              <div className="book__selected--description">
+                <div className="title-container">
+                  <h2 className="book__selected--title">
+                    Crack the coding interview
+                  </h2>
+                  <Rating rating="4.5"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default BookInfo
+export default BookInfo;
