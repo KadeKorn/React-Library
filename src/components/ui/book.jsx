33c00af3c0ea6cf07155519946271d4c564f0aeb
Kade <kadekorn@gmail.com>
2023-09-06 06:34:46 -0700

    test

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
 
-const book = ({ book }) => {
-  function imageLoaded(){
-    console.log('imageloaded')
-  }
+const Book = ({ book }) => {
+  const [imgSrc, setImgSrc] = useState(null);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = book.url;
+
+    imageLoader.onload = () => {
+      setImgSrc(book.url);
+    };
+
+    // Optional: Handle image loading errors
+    imageLoader.onerror = () => {
+      console.error("Failed to load the image:", book.url);
+    };
+  }, [book.url]); // Dependency on book.url ensures this effect runs if the url changes
+
   return (
     <div className="book">
-      <div className="book__img--skeleton"></div>
-      <div className="skeleton book__title--skeleton"></div>
-      <div className="skeleton book__rating--skeleton"></div>
-      <div className="skeleton book__price--skeleton"></div>
-      
-      {/* <Link to={`/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img src={book.url} alt="" onLoad={imageLoaded}/>
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} /> */}
+      {imgSrc ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={imgSrc} alt="" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
     </div>
   );
 };
 
-export default book;
+export default Book;
