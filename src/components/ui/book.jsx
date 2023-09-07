a11b184a1fcf2e0c2735c9884bfba1a6de90c8b1
Kade <kadekorn@gmail.com>
2023-09-07 07:45:57 -0700

    on the Book.jsx I have removed the useRef as it (i speculate) is from a previous version and causes the books to not load. Other than that , I made a few comments

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
@@ -6,31 +6,20 @@ import Price from "./Price";
 const Book = ({ book }) => {
   const [imgSrc, setImgSrc] = useState(null);
 
-  const mountedRef = useRef(true);
-
   useEffect(() => {
     const imageLoader = new Image();
     imageLoader.src = book.url;
     imageLoader.onload = () => {
       setTimeout(() => {
-        if (mountedRef.current) {
-          setImgSrc(imageLoader.src);
-        }
+        setImgSrc(imageLoader.src);
       }, 1500);
-    };
+    }; // The Book component receives a book prop and initializes a state variable imgSrc to manage the source URL of an image. Utilizing the useEffect hook, it preloads the image from the book.url, and once loaded, sets the imgSrc state with a delay of 1.5 seconds; if the image fails to load, an error is logged to the console.
 
     // Optional: Handle image loading errors
     imageLoader.onerror = () => {
       console.error("Failed to load the image:", book.url);
     };
-
-    return () => {
-      // When the component unmounts
-      mountedRef.current = false;
-    };
-}, [book.url]);
-
-
+  }, [book.url]); // Dependency on book.url ensures this effect runs if the url changes
 
   return (
     <div className="book">
@@ -65,3 +54,28 @@ const Book = ({ book }) => {
 };
 
 export default Book;
+
+// ACHTUNG -- This is the code that supposedly correct, but I think the documentation is from an older version as when I use the code below the books do not load
+//   const mountedRef = useRef(true);
+
+//   useEffect(() => {
+//     const imageLoader = new Image();
+//     imageLoader.src = book.url;
+//     imageLoader.onload = () => {
+//       setTimeout(() => {
+//         if (mountedRef.current) {
+//           setImgSrc(imageLoader.src);
+//         }
+//       }, 150);
+//     };
+
+//     // Optional: Handle image loading errors
+//     imageLoader.onerror = () => {
+//       console.error("Failed to load the image:", book.url);
+//     };
+
+//     return () => {
+//       // When the component unmounts
+//       mountedRef.current = false;
+//     };
+// }, [book.url]);
