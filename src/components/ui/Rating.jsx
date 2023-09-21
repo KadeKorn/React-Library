3c9332c78b93bf4c924f2d8e99d09d796171ad4f
Kade <kadekorn@gmail.com>
2023-09-21 07:23:28 -0500

    adding notes and taking time to understand all the functionality

--- src/components/ui/Rating.jsx
+++ src/components/ui/Rating.jsx
@@ -3,8 +3,13 @@ import React from "react";
 
 const Rating = ({ rating }) => {
   return (
-    <div className="book__ratings">
+    <div className="book__ratings"> 
       {new Array(Math.floor(rating)).fill(0).map((_, index) => (
+// new Array and Math.floor are being used to create an array of a length that corresponds to the floored value of rating.
+// the .fill(0) is to fill all elements of the array with 0 so that tge map function can do its thing. In JS empty slots cannot
+// be mapped over, so we beed to fill them
+// Fill(0) is becuase we are creating a new array with the length of Rating it creates an empty array with the length of that 
+//floored rating IE new array(5) // creates an array of 5 empty slots
         <FontAwesomeIcon icon="star" key={index} />
       ))}
       {!Number.isInteger(rating) && <FontAwesomeIcon icon="star-half-alt" />}
